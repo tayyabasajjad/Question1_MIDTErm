fc9b0bc5e3614bb0535034a4007b11c1b54853d3
rayanawais <awaiswaheed@gmail.com>
2021-03-27 15:00:48 +0500

    Swing Lecture Commit

---
+++ SwingJTable/src/swingjtable/StudentTableModel.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingjtable;
+
+import java.util.ArrayList;
+import swingjtable.model.JTableModel;
+import swingjtable.model.Student;
+
+public class StudentTableModel extends JTableModel {
+    String[] columnNames;
+    private Object[][] data;
+
+    public void setColumnNames(String[] col) {
+        columnNames = new String[col.length];
+        for (int j = 0; j < col.length; j++) {
+            columnNames[j] = col[j];
+        }
+        this.setColumnName(columnNames);
+    }
+
+    public void setTableData(ArrayList list) {
+        data = new Object[list.size()][columnNames.length];
+        int row = 0;
+        for (int i = 0; i < list.size(); i++) {
+
+            Student objData = (Student) list.get(i);
+
+            data[row][0] = objData.getStudentRollNum();
+            data[row][1] = objData.getStudentName();
+            data[row][2] = objData.getProgram();
+            
+            row++;
+        }
+        this.setData(data);
+    }
+
+    public void populateTable() {
+        String[] col = {"Student Roll #", "Student Name", "Program"};
+        ArrayList<Student> studentList = new ArrayList<Student>();
+
+        studentList.add(new Student("001", "Awais", "MCS"));
+        studentList.add(new Student("002", "wASEEM", "MCS"));
+
+        setColumnNames(col);
+        setTableData(studentList);
+    }
+}
+
+
